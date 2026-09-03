3c08c8d74d299c477548accf014946bf965ae20a
sc-nikolaoslazaridis <nikolaos.lazaridis@sitecore.com>
2026-09-03 12:55:45 +0300

    chore: testing app layer id injection

--- examples/kit-nextjs-skate-park/src/atoms/TextAtom.tsx
+++ examples/kit-nextjs-skate-park/src/atoms/TextAtom.tsx
@@ -1,7 +1,26 @@
 import { Text } from "@sitecore-content-sdk/nextjs";
 import { TextFieldSchema } from "@sitecore-content-sdk/nextjs/atoms";
+import {useRef, useEffect} from 'react';
 
 export const TextAtom = ({ props }: { props: { text: TextFieldSchema } }) => {
+  // test code to be reverted
   const { text } = props;
-  return <Text field={text} editable={true} />;
+  const compRef = useRef<null | HTMLDivElement>(null)
+  const id = useRef<string | null>(null)
+
+  useEffect(()=>{
+    id.current = window.crypto.randomUUID()
+  },[])
+  
+  useEffect(() => {
+    if (!compRef.current || !id.current) return;
+    
+      const sibling = compRef.current.previousElementSibling
+
+      if (sibling?.tagName.toLowerCase() === 'code') 
+        sibling.setAttribute('data-atom-id', id.current);
+  }, [compRef.current, id.current]);
+      
+
+  return <Text field={text} editable={true} ref={compRef}/>;
 };
